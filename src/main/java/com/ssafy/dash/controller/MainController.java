276a6a3e29da9c56d743112cd5535d47f0d0f4d9
Yoo Seung-jun <funczun@gmail.com>
2025-11-17 14:27:17 +0900

    feat(BE): OAuth 의존성 추가 및 기본 설정

---
+++ src/main/java/com/ssafy/dash/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.ssafy.dash.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+    
+}
