21b59d1384ba0d348395171a5285b77ef93dce0c
Yoo Seung-jun <funczun@gmail.com>
2025-11-23 14:42:12 +0900

    feat(BE): 커뮤니티 보드 API 구현

--- backend/src/main/java/com/ssafy/dash/common/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/dash/common/GlobalExceptionHandler.java
@@ -5,13 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.ssafy.dash.board.exception.BoardNotFoundException;
 import com.ssafy.dash.user.exception.UserNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleNotFound(UserNotFoundException ex) {
+    @ExceptionHandler({UserNotFoundException.class, BoardNotFoundException.class})
+    public ResponseEntity<String> handleNotFound(RuntimeException ex) {
         
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
