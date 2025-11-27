f4528e0e2866a634dccbe6d9fe37f62cf71a3a97
Yoo Seung-jun <funczun@gmail.com>
2025-11-27 16:02:38 +0900

    fix(BE): 웹훅 속성 문제 해결

---
+++ backend/src/main/java/com/ssafy/dash/onboarding/github/GitHubWebhookProperties.java
@@ -0,0 +1,38 @@
+package com.ssafy.dash.onboarding.github;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "github.webhook")
+public class GitHubWebhookProperties {
+
+    private String callbackUrl;
+    private String secret;
+    private String events;
+
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public void setCallbackUrl(String callbackUrl) {
+        this.callbackUrl = callbackUrl;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getEvents() {
+        return events;
+    }
+
+    public void setEvents(String events) {
+        this.events = events;
+    }
+
+}
