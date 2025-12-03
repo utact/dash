7c90d053b97539ba2e6ae7525019e0d8bec59438
Yoo Seung-jun <funczun@gmail.com>
2025-12-03 16:41:02 +0900

    refactor(algorithm): Command DTO 레코드 패턴 적용

--- backend/src/main/java/com/ssafy/dash/algorithm/application/dto/command/AlgorithmRecordUpdateCommand.java
+++ backend/src/main/java/com/ssafy/dash/algorithm/application/dto/command/AlgorithmRecordUpdateCommand.java
@@ -1,15 +1,7 @@
 package com.ssafy.dash.algorithm.application.dto.command;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class AlgorithmRecordUpdateCommand {
-
-    private final String problemNumber;
-    private final String title;
-    private final String language;
-    private final String code;
+public record AlgorithmRecordUpdateCommand(
+        String problemNumber, String title, String language, String code
+) {
 
 }
