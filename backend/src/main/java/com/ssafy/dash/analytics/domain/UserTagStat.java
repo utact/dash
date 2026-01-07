fa8a74eb260028442c36d96492db6616d25ffa39
Yoo Seung-jun <funczun@gmail.com>
2026-01-07 16:39:10 +0900

    chore: import 문 추가 및 빌드 환경 개선

--- backend/src/main/java/com/ssafy/dash/analytics/domain/UserTagStat.java
+++ backend/src/main/java/com/ssafy/dash/analytics/domain/UserTagStat.java
@@ -3,6 +3,8 @@ package com.ssafy.dash.analytics.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
 /**
